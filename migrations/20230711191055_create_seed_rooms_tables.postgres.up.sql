f5b9f84fb514752047a714c630b5c2bb50b3cfe6
AdiAkhileshSingh15 <adiakhilesh.singh.cse21@itbhu.ac.in>
2023-07-12 01:25:26 +0530

    migrations for seeding rooms and restrictions

---
+++ migrations/20230711191055_create_seed_rooms_tables.postgres.up.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.rooms (room_name, created_at, updated_at)
+VALUES (
+        'Major''s Suite',
+        '2023-07-12 00:00:00.000',
+        '2023-07-12 00:00:00.000'
+    ),
+    (
+        'General''s Quarters',
+        '2023-07-12 00:00:00',
+        '2023-07-12 00:00:00'
+    );
---
+++ migrations/20230711191527_create_seed_restrictions_tables.postgres.up.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.restrictions (restriction_name, created_at, updated_at)
+VALUES (
+        'Owner Block',
+        '2023-07-12 00:00:00.000',
+        '2023-07-12 00:00:00.000'
+    ),
+    (
+        'Reservation',
+        '2023-07-12 00:00:00.000',
+        '2023-07-12 00:00:00.000'
+    );
