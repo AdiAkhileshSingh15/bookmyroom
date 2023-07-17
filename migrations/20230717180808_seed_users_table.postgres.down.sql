dd7c8d421b7b49d1e94b9e2b3bc53ea7e4239c03
AdiAkhileshSingh15 <adiakhilesh.singh.cse21@itbhu.ac.in>
2023-07-17 23:55:40 +0530

    Migrations for seeding users table

---
+++ migrations/20230717180808_seed_users_table.postgres.down.sql
@@ -0,0 +1,2 @@
+delete from users
+where email = "adi@singh.com"
---
+++ migrations/20230717180808_seed_users_table.postgres.up.sql
@@ -0,0 +1,18 @@
+INSERT INTO public.users (
+        first_name,
+        last_name,
+        email,
+        password,
+        access_level,
+        created_at,
+        updated_at
+    )
+VALUES (
+        'Adi',
+        'Singh',
+        'adi@singh.com',
+        '$2a$12$OVt1thfvZyvTXJtV/T1mDOnJUeKjppWYgB/Bt/GKJ6aDq7qmis8W2',
+        '3',
+        '2023-07-17 00:00:00.000',
+        '2023-07-17 00:00:00.000'
+    );
