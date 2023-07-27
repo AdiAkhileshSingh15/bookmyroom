811d1fcb027be4d83f4f5ed20030b7e6d9398254
AdiAkhileshSingh15 <adiakhilesh.singh.cse21@itbhu.ac.in>
2023-07-27 15:47:05 +0530

    automated tests setup

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -24,7 +24,6 @@ var app config.AppConfig
 var session *scs.SessionManager
 var infoLog *log.Logger
 var errorLog *log.Logger
-var inTest = false
 
 func main() {
 	db, err := run()
@@ -58,13 +57,18 @@ func run() (*driver.DB, error) {
 	inProduction := flag.Bool("production", true, "Application is in production")
 	useCache := flag.Bool("cache", true, "Use template cache")
 	dbHost := flag.String("dbhost", "localhost", "Database host")
-	dbName := flag.String("dbname", "", "Database name")
-	dbUser := flag.String("dbuser", "", "Database user")
-	dbPass := flag.String("dbpass", "", "Database password")
+	dbName := flag.String("dbname", "bookmyroom", "Database name")
+	dbUser := flag.String("dbuser", "postgres", "Database user")
+	dbPass := flag.String("dbpass", "adi123", "Database password")
 	dbPort := flag.String("dbport", "5432", "Database port")
 	dbSSL := flag.String("dbssl", "disable", "Database ssl settings (disable, prefer, require)")
 
 	flag.Parse()
+
+	if *dbName == "" || *dbUser == "" || *dbPass == "" {
+		fmt.Println("Missing required flags")
+		os.Exit(1)
+	}
 
 	mailChan := make(chan models.MailData)
 	app.MailChan = mailChan
@@ -88,37 +92,29 @@ func run() (*driver.DB, error) {
 	app.Session = session
 
 	// connect to database
-	var db *driver.DB
-	var err error
-	if !inTest {
-		if *dbName == "" || *dbUser == "" || *dbPass == "" {
-			fmt.Println("Missing required flags")
-			os.Exit(1)
-		}
 
-		log.Println("Connecting to database...")
-		connectionString := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=%s", *dbHost, *dbPort, *dbName, *dbUser, *dbPass, *dbSSL)
-		db, err = driver.ConnectSQL(connectionString)
-		if err != nil {
-			log.Fatal("Cannot connect to database! Dying...")
-			return nil, err
-		}
-		log.Println("Connected to database!")
-
-		tc, err := render.CreateTemplateCache()
-		if err != nil {
-			log.Fatal(err)
-			return nil, err
-		}
-
-		app.TemplateCache = tc
-
-		repo := handlers.NewRepo(&app, db)
-		handlers.NewHandlers(repo)
-
-		render.NewRenderer(&app)
-		helpers.NewHelpers(&app)
+	log.Println("Connecting to database...")
+	connectionString := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=%s", *dbHost, *dbPort, *dbName, *dbUser, *dbPass, *dbSSL)
+	db, err := driver.ConnectSQL(connectionString)
+	if err != nil {
+		log.Fatal("Cannot connect to database! Dying...")
+		return nil, err
 	}
+	log.Println("Connected to database!")
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	app.TemplateCache = tc
+
+	repo := handlers.NewRepo(&app, db)
+	handlers.NewHandlers(repo)
+
+	render.NewRenderer(&app)
+	helpers.NewHelpers(&app)
 
 	return db, nil
 }
--- cmd/web/main_test.go
+++ cmd/web/main_test.go
@@ -3,7 +3,6 @@ package main
 import "testing"
 
 func TestRun(t *testing.T) {
-	inTest = true
 	_, err := run()
 	if err != nil {
 		t.Error("Failed to run")
